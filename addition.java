c241d2d52df0308587c61bcd938276bc5f8c19ea
kirthigithubit <kiruthikaraju7@gmail.com>
2024-08-14 13:24:30 +0530

    first commit

---
+++ addition.java
@@ -0,0 +1,18 @@
+public class addition {
+	public static void main(String[] args)
+{
+	System.out.println("hello");
+	System.out.println("ADD,SUB,MUL,DIV,MODU:");
+    int a=100,b=3;
+    int c=a+b;
+    System.out.println("Addition:"+c);
+    c=a-b;
+    System.out.println("Subtraction:"+c);
+    c=a*b;
+    System.out.println("Multiplication:"+c);
+    c=a/b;
+    System.out.println("Division:"+c);
+    c=a%b;
+    System.out.println("modules:"+c);
+	}
+}
